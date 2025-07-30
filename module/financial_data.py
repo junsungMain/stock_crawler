f79ca32ee325445d055386bed71230f731fd6129
junsungMain <eril1094@naver.com>
2025-07-30 20:31:30 +0900

    분기 재무 재표 순서 변경

--- module/financial_data.py
+++ module/financial_data.py
@@ -13,41 +13,41 @@ def get_financial_data(stock_code, session=None):
     for key in response_data:
         if key['title'] == '매출액':
             revenue_data = key['columns']
-            revenue_data = sorted(revenue_data.items(), key=lambda x: x[0])[:4]
+            revenue_data = sorted(revenue_data.items(), key=lambda x: x[0], reverse=True)[1:5]
             for i, (k, v) in enumerate(revenue_data):
                 data[f'매출액(억) | {i+1}'] = float(parse_num_value(v['value']))
             continue
                     
         if key['title'] == '당기순이익':
             net_income_data = key['columns']
-            net_income_data = sorted(net_income_data.items(), key=lambda x: x[0])[:4]
+            net_income_data = sorted(net_income_data.items(), key=lambda x: x[0], reverse=True)[1:5]
             for i, (k, v) in enumerate(net_income_data):
                 data[f'당기순이익(억) | {i+1}'] = float(parse_num_value(v['value']))
             continue
 
         if key['title'] == '부채비율':
             debt_ratio_data = key['columns']
-            debt_ratio_data = sorted(debt_ratio_data.items(), key=lambda x: x[0])[:4]
+            debt_ratio_data = sorted(debt_ratio_data.items(), key=lambda x: x[0], reverse=True)[1:5]
             for i, (k, v) in enumerate(debt_ratio_data):
                 data[f'부채비율(%) | {i+1}'] = float(parse_num_value(v['value']))
             continue
 
         if key['title'] == 'ROE':
             roe_data = key['columns']
-            roe_data = sorted(roe_data.items(), key=lambda x: x[0])[:4]
-            data[f'ROE(%)'] = float(parse_num_value(roe_data[-1][1]['value']))
+            roe_data = sorted(roe_data.items(), key=lambda x: x[0],reverse=True)[1:5]
+            data[f'ROE(%)'] = float(parse_num_value(roe_data[0][1]['value']))
             continue
 
         if key['title'] == 'PBR':
             pbr_data = key['columns']
-            pbr_data = sorted(pbr_data.items(), key=lambda x: x[0])[:4]
-            data[f'PBR(배)'] = float(parse_num_value(pbr_data[-1][1]['value']))
+            pbr_data = sorted(pbr_data.items(), key=lambda x: x[0],reverse=True)[1:5]
+            data[f'PBR(배)'] = float(parse_num_value(pbr_data[0][1]['value']))
             continue
                             
         if key['title'] == 'PER':
             per_data = key['columns']
-            per_data = sorted(per_data.items(), key=lambda x: x[0])[:4]
-            data[f'PER(배)'] = float(parse_num_value(per_data[-1][1]['value']))
+            per_data = sorted(per_data.items(), key=lambda x: x[0],reverse=True)[1:5]
+            data[f'PER(배)'] = float(parse_num_value(per_data[0][1]['value']))
     return data
 
 
@@ -73,12 +73,14 @@ def get_financial_extra_data(stock_code, session=None):
         for key in response_data:
             if key['name'] == '영업이익증가율':
                 profit_data = key['data']
-                for idx, value in enumerate(profit_data):
-                    if idx == 4:
+                for idx, value in enumerate(reversed(profit_data)):
+                    if(idx == 0):
+                        continue
+                    if idx == 5:
                         break
                     if value == 'null':
                         value = '0'
-                    data[f'영업이익증가율(%) | {idx+1}'] = value
+                    data[f'영업이익증가율(%) | {idx}'] = value
                 break;
 
     return data
